b974fbe90d650de0cf2cfe120fffac6ab3f1415c
fathimafemin <fathimafemi9656@gmail.com>
2026-03-30 12:54:11 +0530

    changed db

--- db.py
+++ db.py
@@ -6,16 +6,12 @@ import os
 # Load .env file
 load_dotenv()
 
-# Get values
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
 
 # Create DB URL
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(bind=engine)
 
